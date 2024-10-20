1f7b041213f7871b7485660e3cac8ab4f4e097e8
Anthonyy Choque <giananthoych@outlook.es>
2024-10-19 19:20:42 -0500

    routing service-spaces api-Gateway on the gateway

--- Coworking-Microservices/coworkingSpaces/src/main/java/com/coworking/coworkingSpaces/config/CorsConfig.java
+++ Coworking-Microservices/coworkingSpaces/src/main/java/com/coworking/coworkingSpaces/config/CorsConfig.java
@@ -8,11 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-                .allowedHeaders("*")
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/spaces/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
 }
