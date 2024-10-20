e9430f8b359eb52de3e7a6bec0742e4c13f8baaf
njmn21 <nilsonjmunozn@gmail.com>
2024-10-19 22:35:08 -0500

    cors config

--- Coworking-Microservices/coworkingSpaces/src/main/java/com/coworking/coworkingSpaces/config/CorsConfig.java
+++ Coworking-Microservices/coworkingSpaces/src/main/java/com/coworking/coworkingSpaces/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/spaces/**")
+        registry.addMapping("/spaces/spaces/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
--- Coworking-Microservices/coworkingSpaces/src/main/java/com/coworking/coworkingSpaces/presentation/SpacesController.java
+++ Coworking-Microservices/coworkingSpaces/src/main/java/com/coworking/coworkingSpaces/presentation/SpacesController.java
@@ -3,6 +3,7 @@ package com.coworking.coworkingSpaces.presentation;
 import com.coworking.coworkingSpaces.persistence.entity.SpacesDTO;
 import com.coworking.coworkingSpaces.service.impl.SpacesService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
