e53481e5c1fd3ed224f38f414f32e7d7e22982bb
Anthonyy Choque <giananthoych@outlook.es>
2024-11-24 16:29:15 -0500

    mergin

--- Coworking-Microservices/coworkingSpaces/src/main/java/com/coworking/coworkingSpaces/persistence/repository/SpaceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coworking.coworkingSpaces.persistence.repository;
-
-import com.coworking.coworkingSpaces.persistence.entity.Spaces;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpaceRepository extends JpaRepository<Spaces, Long> {
-}
