e4e6090f745c9d66af7dbf4fb3db76f976587096
Anthonyy Choque <giananthoych@outlook.es>
2024-12-06 13:12:31 -0500

    add url front

--- Coworking-Microservices/membership-service/src/main/java/com/coworking/membership_service/configuration/config/WebMvcConfig.java
+++ Coworking-Microservices/membership-service/src/main/java/com/coworking/membership_service/configuration/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/membership/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173","https://workspace-umber-mu.vercel.app","*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
